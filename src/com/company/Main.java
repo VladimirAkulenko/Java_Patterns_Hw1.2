680d52db85a6d0415d7abf2f39a11f3622457845
Vladimir Akulenko <vladimirakulenk@yandex.ru>
2023-12-10 18:21:26 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.log("Запускаем программу");
+        Scanner scanner = new Scanner(System.in);
+        logger.log("Просим пользователя ввести входные данные для списка");
+        System.out.print("Введите размер списка: ");
+        int size = scanner.nextInt();
+        System.out.print("Введите верхнюю границу для значений: ");
+        int maxValue = scanner.nextInt();
+        logger.log(" Создаём и наполняем список");
+        List<Integer> list = new ArrayList<>(size);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            list.add(random.nextInt(maxValue));
+        }
+        System.out.println("Вот случайный список: " + list);
+        logger.log("Просим пользователя ввести входные данные для фильтрации");
+        System.out.print("Введите порог для фильтра: ");
+        int limit = scanner.nextInt();
+        logger.log("Запускаем фильтрацию");
+        Filter filter = new Filter(limit);
+        List<Integer> result = filter.filterOut(list);
+        logger.log("Выводим результат на экран");
+        System.out.println("Отфильтрованный список: " + result);
+        logger.log("Завершаем программу");
+
+    }
+}
